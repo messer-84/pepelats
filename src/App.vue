08a445647d80881430970270c47848c27c3e2a21
Maksym Vorobiov <maksim.vorobyov@bigdropinc.com>
2018-02-08 18:07:45 +0200

    Deploy in Heroku

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,27 @@
 <template>
 	<div>
-		<top-menu></top-menu>
+		<top-menu v-show="currentRouter != '/'"></top-menu>
 		<router-view></router-view>
 	</div>
 </template>
 
 <script>
   import topMenu from './components/topMenu.vue'
+  import {router} from './app'
 
   export default {
+    data(){
+      return {
+        currentRouter: router.currentRoute.path
+      }
+    },
     components: {
       topMenu
+    },
+    watch: {
+      '$route' (to, from) {
+        this.currentRouter = to.path
+      }
     }
   }
 
